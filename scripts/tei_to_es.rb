8459df507655bea575b92d5e8c140cb7ce1c5852
Karin Dalziel <promo@nirak.net>
2017-08-11 16:02:44 -0500

    altering person to pull from file

--- scripts/tei_to_es.rb
+++ scripts/tei_to_es.rb
@@ -15,9 +15,14 @@ class TeiToEs
   #    GENERAL    #
   #################
 
+  def read_file path
+    Common.create_xml_object("#{@options["coll_dir"]}/#{path}")
+  end
+
   # do something before pulling fields
   def preprocessing
     # read additional files, alter the @xml, add data structures, etc
+    @personography = read_file "authority_files/shanahan_listperson.xml"
   end
 
   # do something after pulling the fields
@@ -42,6 +47,34 @@ class TeiToEs
   # def self.fieldname
   #   your custom code
   # end
+
+  def person
+    list = []
+    people = @xml.xpath(@xpaths["person"])
+    people.each do |p|
+      xmlid = p["id"]
+      role = p["type"]
+
+      # collect the parts of the person's name
+      person = @personography.xpath("//person[@id='#{xmlid}']")
+      nameFull = person.xpath("persName[@type='display']").text
+      nameSqueezed = Common.squeeze(nameFull)
+      # birth = person.xpath("ab/persName/surname[@type='birth']").text
+      # they may have zero or many additional names, so iterate through
+      # add = person.xpath("ab/persName/addName").map { |n| n.text }.join(", ")
+
+      # put together the name itself
+      label = nameSqueezed
+
+      pers_obj = { "name" => label, "id" => xmlid }
+      pers_obj["role"] = role if role
+      list << pers_obj
+
+      # some of the personography entries are also tied to annotations, keep track of them
+      #@person_annos << person.xpath("ab/ref[@type='annotation']/@target").text
+    end
+    return list.uniq
+  end
 
   # In the below example, the normal "person" behavior is customized
   def self.person
