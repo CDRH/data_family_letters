9d9a9dd2abc440aef12163ef51187438a5cd049e
Jessica Dussault <jdussault4@gmail.com>
2020-08-24 12:00:43 -0500

    adding more map generation

--- scripts/overrides/file_custom.rb
+++ scripts/overrides/file_custom.rb
@@ -27,7 +27,9 @@ class FileCustom < FileType
     FileUtils.mkdir_p(@output_dir)
 
     create_photographs
-    create_from_city_densities
+    create_city_densities_destination
+    create_city_densities_origin
+    create_letter_route
     # just send something blank back to datura
     # to make it happy
     {}
@@ -38,71 +40,112 @@ class FileCustom < FileType
 
   private
 
-  def create_photographs
-    items = @file.select { |item| item["subcategory"] == "Photograph" }
-    geojson = items_to_geojson(items)
-    write_geojson("photographs.json", geojson)
-  end
-
-  def create_from_city_densities
-    # collect all letters
-    # aggregate cities sending letters and amounts
-    # create point layers with overall density
-    # create line layers with number info
-    # TODO break down by decade?
+  # creates points with count information for number of incoming or outgoing
+  # letters, whichever is selected by "letter_method" [ aka letter.origin or
+  # letter.destination ]
+  # letter_method
+  #   "origin" or "destination"
+  # file_out
+  #   "cities_from.json" or "cities_to.json"
+  def create_city_densities(letter_method, file_out)
     items = @file.select { |item| item["subcategory"] == "Letter" }
     cities_total = {}
-    cities_from_to = {}
 
     # create a feature for each city and increase a count prop
     items.each do |item|
       letter = Letter.new(item)
-      next if !letter.origin
+      type = letter.send(letter_method)
+      next if !type
 
-      place = letter.origin["title"]
+      place = type["title"]
+      es = (item["language"]) == "es" ? 1 : 0
+      en = item["language"] == "en" ? 1 : 0
       if !cities_total.key?(place)
         cities_total[place] = {
           "type" => "Feature",
           "properties" => {
             "count" => 1,
+            "es" => es,
+            "en" => en,
             "location" => place,
             "letters" => [ letter.properties ]
           },
-          "geometry" => letter.origin_geometry
+          "geometry" => letter.send("#{letter_method}_geometry")
         }
       else
-        cities_total[place]["properties"]["count"] += 1
-        cities_total[place]["properties"]["letters"] << letter.properties
+        props = cities_total[place]["properties"]
+        props["count"] += 1
+        props["es"] += es
+        props["en"] += en
+        props["letters"] << letter.properties
       end
     end
-
-    # iterate through cities_total and calculate
-    # percentage of primarily spanish letters
 
     json = {
       "type" => "FeatureCollection",
       "features" => cities_total.values
     }
-    write_geojson("city_from.json", json)
-    # items.each do |item|
+    write_geojson(file_out, json)
+  end
 
-    #   next if !from || !to
+  def create_city_densities_destination
+    create_city_densities("destination", "city_to.json")
+  end
 
-    #   place = from["title"]
-    #   if !cities_total.key?(place)
-    #     cities_total[place] = 0
-    #     # assuming this also hasn't been created
-    #     cities_from_to[place] = {}
-    #   end
-    #   cities_total[place] += 1
-    #   if !cities_from_to[place].key?(to["title"])
-    #     cities_from_to[place][to["title"]] = 0
-    #   end
-    #   cities_from_to[place][to["title"]] += 1
-    # end
-    # puts cities_total
-    # puts "--"
-    # puts cities_from_to
+  def create_city_densities_origin
+    create_city_densities("origin", "city_from.json")
+  end
+
+  def create_letter_route
+    items = @file.select { |item| item["subcategory"] == "Letter" }
+    # holds city_from|city_to with pipe delineator
+    routes_total = {}
+
+    # create a feature for each city and increase a count prop
+    items.each do |item|
+      letter = Letter.new(item)
+      origin = letter.origin
+      dest = letter.destination
+      next if !origin || !dest
+
+      place_from = origin["title"]
+      place_to = dest["title"]
+      key = "#{place_from}|#{place_to}"
+
+      es = (item["language"]) == "es" ? 1 : 0
+      en = item["language"] == "en" ? 1 : 0
+      if routes_total.key?(key)
+        props = routes_total[key]["properties"]
+        props["count"] += 1
+        props["es"] += 1
+        props["en"] += 1
+        props["letters"] << letter.properties
+      else
+        routes_total[key] = {
+          "type" => "Feature",
+          "properties" => {
+            "count" => 1,
+            "es" => es,
+            "en" => en,
+            "location" => "#{place_from} to #{place_to}",
+            "letters" => [ letter.properties ]
+          },
+          "geometry" => letter.route_geometry
+        }
+      end
+    end
+
+    json = {
+      "type" => "FeatureCollection",
+      "features" => routes_total.values
+    }
+    write_geojson("routes_all.json", json)
+  end
+
+  def create_photographs
+    items = @file.select { |item| item["subcategory"] == "Photograph" }
+    geojson = items_to_geojson(items)
+    write_geojson("photographs.json", geojson)
   end
 
   def items_to_geojson(items)
